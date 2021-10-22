d56e47a5b0ff8697cd4c4edf4ed7d5dc7c3fdeaf
Yves Hurdequint <hurdequinty@hotmail.com>
2021-10-22 09:26:20 +0200

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from './components/create-todo';
+import EditTodo from './components/edit-todo';
+import TodosList from './components/todos-list';
+
+import logo from './assets/images/logo.JPG';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          <img src={logo} alt="" style={{width: "60px"}} />
+        </Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">MERN-Stack todo app</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/create">Create Todo</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+        <Route path="/" exact component={TodosList} />
+        <Route path="edit/:id" exact component={EditTodo} />
+        <Route path="/create" exact component={CreateTodo} />
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/create-todo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateTodo extends Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to the create-todo component</p>
+      </div>
+    )
+  }
+}
+
+export default CreateTodo;
